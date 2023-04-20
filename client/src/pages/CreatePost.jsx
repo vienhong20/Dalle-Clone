c9ac4af8bf453ee6fc0a6364a4075f84ba2d43f2
VIEN HONG <vienhong88@gmail.com>
2023-04-20 16:26:48 -0400

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,13 +15,48 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [Loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
+  const handleSurpriseMe = () => {
+    
+  }
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
             <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
             <p className="mt-2 text-[#666e75] text-[14px] max-w[500px]">Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
       </div>
-      
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField 
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+           <FormField 
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+
     </section>
   )
 }
