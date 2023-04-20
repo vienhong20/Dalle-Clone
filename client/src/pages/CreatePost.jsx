bc5aecd0d5fe01461095ef5694a3e402ab7cb602
VIEN HONG <vienhong88@gmail.com>
2023-04-20 17:02:15 -0400

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -12,6 +12,7 @@ const CreatePost = () => {
     prompt:'',
     photo:'',
   });
+
   const [generatingImg, setGeneratingImg] = useState(false);
   const [Loading, setLoading] = useState(false);
 
@@ -54,9 +55,23 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img 
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"  
+              />
+            ) : (
+              <img 
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+          </div>
         </div>
       </form>
-
     </section>
   )
 }
