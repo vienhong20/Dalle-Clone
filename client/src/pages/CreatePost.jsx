3173f3389d9b649f95c07abba8e7c448254e25c7
VIEN HONG <vienhong88@gmail.com>
2023-04-20 18:00:21 -0400

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -91,6 +91,18 @@ const CreatePost = () => {
             {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">Once you have created the image you want, you can share it with others in the community
+          </p>
+          <button 
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {Loading ? 'Loading...' : 'Share with the community'}
+          </button>
+        
+        </div>
       </form>
     </section>
   )
