05406dd78959d8edfcf16eec79b72ada1b3390b8
VIEN HONG <vienhong88@gmail.com>
2023-04-25 15:12:02 -0400

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,8 +16,29 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [Loading, setLoading] = useState(false);
 
-  const generateImage = () => {
+  const generateImage = async () => {
+    if(form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response =await fetch('http:localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        })
 
+        const data = await response.json();
+
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}`})
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt')
+    }
   }
 
   const handleSubmit = () => {
