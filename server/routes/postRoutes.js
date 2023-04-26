3426c2285a5181ddc70d93cf7ef8f5088ad3b2e4
VIEN HONG <vienhong88@gmail.com>
2023-04-26 12:54:44 -0400

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -16,7 +16,13 @@ cloudinary.config({
 
 //GET ALL POSTS
 router.route('/').get(async(req, res) => {
+    try {
+        const posts = await Post.find({});
 
+        res.status(200).json({ success: true, data: posts })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
 });
 
 //CREATE A POST
