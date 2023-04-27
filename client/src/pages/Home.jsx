bca4bae4ffa6020f42a48a4d1f2b3340aac70ebe
VIEN HONG <vienhong88@gmail.com>
2023-04-27 17:38:06 -0400

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,6 +16,32 @@ const Home = () => {
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState(null);
 
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+
+            try{
+                const response = await fetch('http://localhost:8080/api/v1/post',{
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+
+                if(response.ok) {
+                    const result = await response.json();
+
+                    setAllPosts(result.data.reserve());
+                }
+            } catch (error) {
+
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchPosts();
+    }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
         <div>
@@ -47,7 +73,7 @@ const Home = () => {
                         />
                     ) : (
                         <RenderCards 
-                            data={[]}
+                            data={[allPosts]}
                             title="No posts found"
                         />
                     )}
