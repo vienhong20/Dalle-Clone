d2988eb877361f382b371b3778ace59c400d1604
VIEN HONG <vienhong88@gmail.com>
2023-04-27 17:29:35 -0400

    Update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -42,8 +42,31 @@ const CreatePost = () => {
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    if(form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://loaclhost:8080/api/v1/post',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form)
+        })
+
+        await response.json();
+        navigate('/');
+      } catch (err) {
+        alert(err)
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please enter a prompt nad generate an image')
+    }
   }
 
   const handleChange = (e) => {
